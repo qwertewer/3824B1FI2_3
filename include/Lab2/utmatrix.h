939e83ee6901561ba3dcafb5b97fb9a7d0e56e5d
Mercurial00 <danil.starostin.2006@mail.ru>
2025-11-13 21:47:40 +0300

    include

--- include/Lab2/utmatrix.h
+++ include/Lab2/utmatrix.h
@@ -10,34 +10,34 @@ template <class ValType>
 class TVector
 {
 protected:
-	int Size;       // размер вектора
-	int StartIndex; // индекс первого элемента вектора
-	ValType* pVector;
+  int Size;       // размер вектора
+  int StartIndex; // индекс первого элемента вектора
+  ValType* pVector;
 public:
-	TVector(int s = 10, int si = 0);
-	TVector(const TVector& v);                // конструктор копирования
-	~TVector();
-	int GetSize() { return Size; } // размер вектора
-	int GetStartIndex() { return StartIndex; } // индекс первого элемента
-	ValType& operator[](int pos);             // доступ
-	bool operator==(const TVector& v) const;  // сравнение
-	bool operator!=(const TVector& v) const;  // сравнение
-	TVector& operator=(const TVector& v);     // присваивание
+  TVector(int s = 10, int si = 0);
+  TVector(const TVector &v);                // конструктор копирования
+  ~TVector();
+  int GetSize()      { return Size;       } // размер вектора
+  int GetStartIndex(){ return StartIndex; } // индекс первого элемента
+  ValType& operator[](int pos);             // доступ
+  bool operator==(const TVector &v) const;  // сравнение
+  bool operator!=(const TVector &v) const;  // сравнение
+  TVector& operator=(const TVector &v);     // присваивание
 
-	// скалярные операции
-	TVector  operator+(const ValType& val);   // прибавить скаляр
-	TVector  operator-(const ValType& val);   // вычесть скаляр
-	TVector  operator*(const ValType& val);   // умножить на скаляр
+  // скалярные операции
+  TVector  operator+(const ValType &val);   // прибавить скаляр
+  TVector  operator-(const ValType &val);   // вычесть скаляр
+  TVector  operator*(const ValType &val);   // умножить на скаляр
 
-	// векторные операции
-	TVector  operator+(const TVector& v);     // сложение
-	TVector  operator-(const TVector& v);     // вычитание
-	ValType  operator*(const TVector& v);     // скалярное произведение
+  // векторные операции
+  TVector  operator+(const TVector &v);     // сложение
+  TVector  operator-(const TVector &v);     // вычитание
+  ValType  operator*(const TVector &v);     // скалярное произведение
 
-	// ввод-вывод
+  // ввод-вывод
 
-	friend std::istream& operator>>(std::istream& in, TVector& v);
-	friend std::ostream& operator<<(std::ostream& out, const TVector& v);
+  friend std::istream& operator>>(std::istream& in, TVector& v);
+  friend std::ostream& operator<<(std::ostream& out, const TVector& v);
 };
 
 // Верхнетреугольная матрица
@@ -45,16 +45,16 @@ template <class ValType>
 class TMatrix : public TVector<TVector<ValType>>
 {
 public:
-	TMatrix(int s = 10);
-	TMatrix(const TMatrix& mt);                    // копирование
-	TMatrix(const TVector<TVector<ValType>>& mt);  // преобразование типа
-	bool operator==(const TMatrix& mt) const;      // сравнение
-	bool operator!=(const TMatrix& mt) const;      // сравнение
-	TMatrix& operator= (const TMatrix& mt);        // присваивание
-	TMatrix  operator+ (const TMatrix& mt);        // сложение
-	TMatrix  operator- (const TMatrix& mt);        // вычитание
+  TMatrix(int s = 10);                           
+  TMatrix(const TMatrix &mt);                    // копирование
+  TMatrix(const TVector<TVector<ValType>> &mt);  // преобразование типа
+  bool operator==(const TMatrix &mt) const;      // сравнение
+  bool operator!=(const TMatrix &mt) const;      // сравнение
+  TMatrix& operator= (const TMatrix &mt);        // присваивание
+  TMatrix  operator+ (const TMatrix &mt);        // сложение
+  TMatrix  operator- (const TMatrix &mt);        // вычитание
 
-	// ввод / вывод
-	friend std::istream& operator>>(std::istream& in, TMatrix& mt);
-	friend std::ostream& operator<<(std::ostream& out, const TMatrix& mt);
+  // ввод / вывод
+  friend std::istream& operator>>(std::istream& in, TMatrix& mt);
+  friend std::ostream& operator<<(std::ostream& out, const TMatrix& mt);
 };
